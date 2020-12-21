c5dc2ed0be22b24e1ada2df7616c9e416aa6e975
Ismaël <ismael.elkihel@gmail.com>
2020-12-21 15:44:20 +0000

    - Seems that Epic trolled me, modifying the values - Added the game link, generated from info on the same JSON - hmm...

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if(msg.content === 'epic.freegame') {
-    const body = fetch("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR")
+    const body = fetch("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions")
     .then((stock) => {
     return stock.json()
     }).then(json => {
-    var freeGame = json.data.Catalog.searchStore.elements[0].title;
-    msg.reply("Today's free game is " + freeGame + "\nGo get it when you can !")
+    var freeGame = json.data.Catalog.searchStore.elements[2].title;
+    var pSlugLink = json.data.Catalog.searchStore.elements[2].customAttributes[5].value;
+    var nextGame = json.data.Catalog.searchStore.elements[3].title;
+    var npSlugLink = json.data.Catalog.searchStore.elements[3].customAttributes[6].value;
+    var link = "https://www.epicgames.com/store/product/" + pSlugLink + "/home"
+    var otlink = "https://www.epicgames.com/store/product/" + npSlugLink + "/home"
+    msg.reply("Today's free game is " + freeGame + " until 5:00 PM UTC+1.\nLink available here : " + link + ".\nNext game is " + nextGame + ".\nLink (open at 5:00 PM UTC+1) : " + otlink)
     });
   }
 });
