bd81c8dcbd2fa5a3c0a9f5721ffa1de91367f87f
Ismaël <ismael.elkihel@gmail.com>
2020-12-22 18:20:32 +0000

    Implemented new way of doing it.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
+
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
@@ -16,13 +17,39 @@ bot.on('message', msg => {
     .then((stock) => {
     return stock.json()
     }).then(json => {
-    var freeGame = json.data.Catalog.searchStore.elements[2].title;
-    var pSlugLink = json.data.Catalog.searchStore.elements[2].customAttributes[5].value;
-    var nextGame = json.data.Catalog.searchStore.elements[3].title;
-    var npSlugLink = json.data.Catalog.searchStore.elements[3].customAttributes[6].value;
-    var link = "https://www.epicgames.com/store/product/" + pSlugLink + "/home"
-    var otlink = "https://www.epicgames.com/store/product/" + npSlugLink + "/home"
-    msg.reply("Today's free game is " + freeGame + " until 5:00 PM UTC+1.\nLink available here : " + link + ".\nNext game is " + nextGame + ".\nLink (open at 5:00 PM UTC+1) : " + otlink)
+      var date = new Date();
+      var day  = date.getDate();
+      var nextDay = day + 1;
+      var constructedDate = "december" + day + "mysterygame";
+      var constructedTDate = "december" + nextDay + "mysterygame";
+      var todaysfreegame = json.data.Catalog.searchStore.elements.find(it =>
+      {
+      return it.urlSlug == constructedDate;
+      });
+      var tomorrowsfreegame = json.data.Catalog.searchStore.elements.find(it =>
+      {
+      return it.urlSlug == constructedTDate;
+      });
+      var freeGame = todaysfreegame.title;
+      var productSlug = todaysfreegame.customAttributes.find(it =>
+      {
+      return it.key == 'com.epicgames.app.productSlug';
+      });
+      var nextGame = tomorrowsfreegame.title;
+      var nextproductSlug = tomorrowsfreegame.customAttributes.find(it =>
+      {
+      return it.key == 'com.epicgames.app.productSlug';
+      });
+      var trimmedPSlug = productSlug.value.replace('/home', '');
+      var trimmedNPSlug = nextproductSlug.value.replace('/home', '');
+      var link = "https://www.epicgames.com/store/product/" + trimmedPSlug + "/home"
+      var otlink = "https://www.epicgames.com/store/product/" + trimmedNPSlug + "/home"
+      if(nextGame == "Mystery Game"){
+        msg.reply("Today's free game is " + freeGame + " until 5:00 PM UTC+1.\nLink available here : " + link + ".\nNext game isn't leaked yet, you will need to wait !")
+      }
+      else {
+        msg.reply("Today's free game is " + freeGame + " until 5:00 PM UTC+1.\nLink available here : " + link + ".\nNext game is " + nextGame + ".\nLink (open at 5:00 PM UTC+1) : " + otlink)
+      }
     });
   }
 });
