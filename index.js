81ebc46aa15197d4e0cfa8b34de5e808c21554dc
Shadix A <30985701+LaneSh4d0w@users.noreply.github.com>
2021-11-30 19:21:19 +0000

    code : delete dates on query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env['TOKEN'];
 
 // Make the bot log in using the token you've put in your .env file
 bot.login(TOKEN);
@@ -20,7 +20,7 @@ bot.on('message', msg => {
 
   if(command === '.games') {
     // GraphQL body
-    const query = {"query":"query searchStoreQuery($allowCountries: String, $category: String, $count: Int, $country: String!, $keywords: String, $locale: String, $namespace: String, $itemNs: String, $sortBy: String, $sortDir: String, $start: Int, $tag: String, $releaseDate: String, $withPrice: Boolean = false, $withPromotions: Boolean = false, $priceRange: String, $freeGame: Boolean, $onSale: Boolean, $effectiveDate: String) {\n  Catalog {\n    searchStore(\n      allowCountries: $allowCountries\n      category: $category\n      count: $count\n      country: $country\n      keywords: $keywords\n      locale: $locale\n      namespace: $namespace\n      itemNs: $itemNs\n      sortBy: $sortBy\n      sortDir: $sortDir\n      releaseDate: $releaseDate\n      start: $start\n      tag: $tag\n      priceRange: $priceRange\n      freeGame: $freeGame\n      onSale: $onSale\n      effectiveDate: $effectiveDate\n    ) {\n      elements {\n        title\n        id\n        namespace\n        description\n        effectiveDate\n        keyImages {\n          type\n          url\n        }\n        currentPrice\n        seller {\n          id\n          name\n        }\n        productSlug\n        urlSlug\n        url\n        tags {\n          id\n        }\n        items {\n          id\n          namespace\n        }\n        customAttributes {\n          key\n          value\n        }\n        categories {\n          path\n        }\n        price(country: $country) @include(if: $withPrice) {\n          totalPrice {\n            discountPrice\n            originalPrice\n            voucherDiscount\n            discount\n            currencyCode\n            currencyInfo {\n              decimals\n            }\n            fmtPrice(locale: $locale) {\n              originalPrice\n              discountPrice\n              intermediatePrice\n            }\n          }\n          lineOffers {\n            appliedRules {\n              id\n              endDate\n              discountSetting {\n                discountType\n              }\n            }\n          }\n        }\n        promotions(category: $category) @include(if: $withPromotions) {\n          promotionalOffers {\n            promotionalOffers {\n              startDate\n              endDate\n              discountSetting {\n                discountType\n                discountPercentage\n              }\n            }\n          }\n          upcomingPromotionalOffers {\n            promotionalOffers {\n              startDate\n              endDate\n              discountSetting {\n                discountType\n                discountPercentage\n              }\n            }\n          }\n        }\n      }\n      paging {\n        count\n        total\n      }\n    }\n  }\n}\n","variables":{"category":"games/edition/base","count": 100,"country":"FR","keywords":"","locale":"fr","sortBy":"releaseDate","sortDir":"DESC","allowCountries":"FR","start":0,"tag":"","releaseDate":"[,2021-01-02T14:32:38.375Z]","freeGame":true,"effectiveDate":"[,2021-01-02T14:32:38.375Z]","withPrice":true}}
+    const query = {"query":"query searchStoreQuery($allowCountries: String, $category: String, $count: Int, $country: String!, $keywords: String, $locale: String, $namespace: String, $itemNs: String, $sortBy: String, $sortDir: String, $start: Int, $tag: String, $releaseDate: String, $withPrice: Boolean = false, $withPromotions: Boolean = false, $priceRange: String, $freeGame: Boolean, $onSale: Boolean, $effectiveDate: String) {\n  Catalog {\n    searchStore(\n      allowCountries: $allowCountries\n      category: $category\n      count: $count\n      country: $country\n      keywords: $keywords\n      locale: $locale\n      namespace: $namespace\n      itemNs: $itemNs\n      sortBy: $sortBy\n      sortDir: $sortDir\n      releaseDate: $releaseDate\n      start: $start\n      tag: $tag\n      priceRange: $priceRange\n      freeGame: $freeGame\n      onSale: $onSale\n      effectiveDate: $effectiveDate\n    ) {\n      elements {\n        title\n        id\n        namespace\n        description\n        effectiveDate\n        keyImages {\n          type\n          url\n        }\n        currentPrice\n        seller {\n          id\n          name\n        }\n        productSlug\n        urlSlug\n        url\n        tags {\n          id\n        }\n        items {\n          id\n          namespace\n        }\n        customAttributes {\n          key\n          value\n        }\n        categories {\n          path\n        }\n        price(country: $country) @include(if: $withPrice) {\n          totalPrice {\n            discountPrice\n            originalPrice\n            voucherDiscount\n            discount\n            currencyCode\n            currencyInfo {\n              decimals\n            }\n            fmtPrice(locale: $locale) {\n              originalPrice\n              discountPrice\n              intermediatePrice\n            }\n          }\n          lineOffers {\n            appliedRules {\n              id\n              endDate\n              discountSetting {\n                discountType\n              }\n            }\n          }\n        }\n        promotions(category: $category) @include(if: $withPromotions) {\n          promotionalOffers {\n            promotionalOffers {\n              startDate\n              endDate\n              discountSetting {\n                discountType\n                discountPercentage\n              }\n            }\n          }\n          upcomingPromotionalOffers {\n            promotionalOffers {\n              startDate\n              endDate\n              discountSetting {\n                discountType\n                discountPercentage\n              }\n            }\n          }\n        }\n      }\n      paging {\n        count\n        total\n      }\n    }\n  }\n}\n","variables":{"category":"games/edition/base|bundles/games|editors|software/edition/base","count": 100,"country":"FR","keywords":"","locale":"fr","sortBy":"releaseDate","sortDir":"DESC","allowCountries":"FR","start":0,"tag":"","freeGame":true,"withPrice":true,"withMapping": true}}
     // Fetch data from Epic Games' API
     fetch("https://www.epicgames.com/graphql", {
       method: 'POST',
@@ -71,13 +71,12 @@ bot.on('message', msg => {
     .then(json => {
       if (json.data.Catalog.searchStore.elements[0].title === undefined) {
           msg.reply('No product has been found with this name.')
-      }
-      else {
+      } else if (json.data.Catalog.searchStore.elements[0].productSlug !== null) {
         const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle(json.data.Catalog.searchStore.elements[0].title)
         .setURL("https://www.epicgames.com/store/product/" + json.data.Catalog.searchStore.elements[0].productSlug.replace('/home', '') + "/home")
-        .setAuthor('Epic Free Game Watcher', 'https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png', 'https://github.com/LaneSh4d0w/Epic_FreeGame_Watcher_Discord')
+        .setAuthor('Epic Games Watcher', 'https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png', 'https://github.com/LaneSh4d0w/Epic_FreeGame_Watcher_Discord')
         .setDescription('Found your requested product !')
         .setThumbnail(json.data.Catalog.searchStore.elements[0].keyImages[3].url)
         .addFields(
@@ -86,8 +85,21 @@ bot.on('message', msg => {
         )
         .setTimestamp()
         .setFooter('Bot made by Lane_Sh4d0w', 'https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png');
-
-      msg.reply(exampleEmbed);
+        msg.reply(exampleEmbed);
+      } else {
+         const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(json.data.Catalog.searchStore.elements[0].title)
+        .setAuthor('Epic Games Watcher', 'https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png', 'https://github.com/LaneSh4d0w/Epic_FreeGame_Watcher_Discord')
+        .setDescription('Found your requested product !')
+        .setThumbnail(json.data.Catalog.searchStore.elements[0].keyImages[3].url)
+        .addFields(
+          { name: 'Price', value: json.data.Catalog.searchStore.elements[0].price.totalPrice.fmtPrice.originalPrice},
+          { name: 'Developer', value: json.data.Catalog.searchStore.elements[0].seller.name},
+        )
+        .setTimestamp()
+        .setFooter('Bot made by Lane_Sh4d0w', 'https://cdn2.unrealengine.com/Unreal+Engine%2Feg-logo-filled-1255x1272-0eb9d144a0f981d1cbaaa1eb957de7a3207b31bb.png');
+        msg.reply(exampleEmbed);
       }
     });
     }
